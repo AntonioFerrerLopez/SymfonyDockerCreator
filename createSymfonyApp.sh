f88179cbd960ce6052054e40be6f40a7be761541
Antonio <buceocv@gmail.com>
2021-08-06 16:29:03 +0200

    Reorganizado para que se copien las configuraciones de docker a la aplicación , de esta forma se puede hacer uso de los servicios

--- createSymfonyApp.sh
+++ createSymfonyApp.sh
@@ -2,13 +2,23 @@
 # -*- ENCODING: UTF-8 -*-
 
 echo -e '\n'
-echo -e '\e[1;37m ACCIÓN PREVENTIVA: DETENIENDO CONTENEDORES \e[0m \n'
+echo -e '\e[1;37m ACCIÓN PREVENTIVA: DETENIENDO Y ELIMINANDO CONTENEDORES \e[0m \n'
 docker stop php 
-docker stop symfony-mysql
 docker stop symfony-nginx
+docker stop symfony-mariadb
+docker stop symfony-adminer
+docker stop symfony-rabbitmq
+docker stop symfony-portainer
+
+docker rm php 
+docker rm symfony-nginx
+docker rm symfony-mariadb
+docker rm symfony-adminer
+docker rm symfony-rabbitmq
+docker rm symfony-portainer
 
 echo -e '\n'
-echo -e '\e[1;34m INICIANDO DESPLIEGE \e[0m \n'
+echo -e '\e[1;34m INICIANDO DESPLIEGE y comprobando el correcto despliegue de los contenedores base \e[0m \n'
 docker-compose up -d --build 
 
 if [ $? != 0 ];
@@ -33,29 +43,74 @@ else
 fi
 
 
-echo -e '\e[1;34m CREADO EL PROYECTO \e[0m \n'
+echo -e '\e[1;34m CREADO EL PROYECTO. Esto puede tardar un poco ... \e[0m \n'
 docker exec php symfony new . 
 
 if [ $? != 0 ];
 then
-   echo -e '\e[1;31m ERROR AL CREAR PROYECTO ¿la carpeta ./app está vacia?  \e[0m \n'
+   echo -e '\e[1;31m ERROR AL CREAR PROYECTO ¿la carpeta ./app está vacia? , Dispone de conexión a internet?  \e[0m \n'
     exit 1
    
 else
-  echo -e '\n \e[1;32m FIN DEL PROCESO DE CREACIÓN. listo !!!  revise la carpeta ./app \e[0m \n'
+  echo -e '\n \e[1;32m FIN DEL PROCESO DE CREACIÓN. listo !!!  revise la carpeta ./app \e[0m'
   echo -e '\e[0;36m Puede acceder al proyecto desde http:://localhost:8080 \e[0m \n'
 fi
 
-echo -e '\n \e[1;32m COPIANDO docker-composer.yaml con servicios básicos  en APP >> ./app \e[0m \n'
-cp infrastructure/Symfony/docker-compose.yaml app/
+
+echo -e '\n'
+echo -e '\e[1;37m PARANDO y ELIMIANADO SERVICIOS DE DOCKER  (ya se pueden emplear desde la aplicación)\e[0m \n'
+
+
+docker stop php 
+docker stop symfony-nginx
+docker stop symfony-mariadb
+docker stop symfony-adminer
+docker stop symfony-rabbitmq
+docker stop symfony-portainer
+
+docker rm php 
+docker rm symfony-nginx
+docker rm symfony-mariadb
+docker rm symfony-adminer
+docker rm symfony-rabbitmq
+docker rm symfony-portainer
+
+echo -e '\n'
+echo -e '\e[1;37m ELIMINANDO VOLUMEN DE MARIADB provisional\e[0m \n'
+rm -R infrastructure/databases
+
+
+echo -e '\n \e[1;34m PASANDO CONFIGURACIÓN DE CONTENEDORES A APLICACIÓN ./app \e[0m \n'
+
+mkdir app/docker
+
+cp docker-compose.yaml app/docker
 if [ $? != 0 ];
 then
-   echo -e '\e[1;31m ERROR AL COPIAR: infrastructure/Symfony/docker-compose.yaml. CONTINUANDO CON EL PROCESO \e[0m \n'
+   echo -e '\e[1;31m ERROR AL COPIAR: docker-compose.yaml a app/docker revise la configuración una vez termine el proceso CONTINUANDO... \e[0m \n'
 
 else
-  echo -e '\n \e[1;32m TODO LISTO \e[0m \n'
+  echo -e '\n \e[1;32m copiando valiables de entorno para docker \e[0m \n'
 fi
 
+cp .env app/docker
+if [ $? != 0 ];
+then
+     echo -e '\e[1;31m ERROR AL COPIAR: .env a app/docker revise la configuración una vez termine el proceso CONTINUANDO... \e[0m \n'
+
+else
+  echo -e '\n \e[1;32m copiado correctamente \e[0m \n'
+fi
+
+echo -e '\n \e[1;34m COPIANDO CONFIGURACIÓN DE los contenedores \e[0m \n'
+cp -r ./infrastructure/ app/docker/
+if [ $? != 0 ];
+then
+     echo -e '\e[1;31m ERROR AL COPIAR CONFIGURACIÓN DE CONTENEDORES:  revise la configuración una vez termine el proceso CONTINUANDO... \e[0m \n'
+else
+  echo -e '\n \e[1;32m Configuración del contenedores copiada correctamente \e[0m \n'
+fi
+  
 
 
 
